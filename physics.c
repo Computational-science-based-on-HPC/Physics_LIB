ace1a1de13223863755414560b2cebca162a6b8b
Jonathan Ghaly <60841107+JonathanGhaly@users.noreply.github.com>
2023-06-19 23:33:13 +0200

    Fixed elastic pendulum

--- physics.c
+++ physics.c
@@ -1,43 +1,45 @@
 #include "physics.h"
 #include "oscserial.h"
 #include "oscpara.h"
+
 int
 damped_os_serial(double max_amplitude, double length, double mass, double gravity, double k, double Ao, double Vo,
                  double FI,
                  double time_limit, double step_size, double damping_coefficent, int number_of_files)
 {
     return
-            _simulate_damped_os_serial(max_amplitude, length, mass, gravity, k, Ao, Vo,FI,
-                                      time_limit, step_size, damping_coefficent, number_of_files);
+            _simulate_damped_os_serial(max_amplitude, length, mass, gravity, k, Ao, Vo, FI,
+                                       time_limit, step_size, damping_coefficent, number_of_files);
 }
 
 int
 damped_os_parallel_v1(double max_amplitude, double length, double mass, double gravity, double k, double Ao, double Vo,
-                   double FI,
-                   double time_limit, double step_size, double damping_coefficent, int number_of_files)
+                      double FI,
+                      double time_limit, double step_size, double damping_coefficent, int number_of_files)
 {
 #ifdef _WIN32
     return
             _simulate_damped_os_serial(max_amplitude, length, mass, gravity, k, Ao, Vo, FI,
-                                      time_limit, step_size, damping_coefficent, number_of_files);
+                                       time_limit, step_size, damping_coefficent, number_of_files);
 #endif
     return _simulate_damped_os_parallel_mpi_omp(max_amplitude, length, mass, gravity, k, Ao, Vo, FI,
-                                      time_limit, step_size, damping_coefficent, number_of_files);
+                                                time_limit, step_size, damping_coefficent, number_of_files);
 }
 
 int
 damped_os_parallel_v2(double max_amplitude, double length, double mass, double gravity, double k, double Ao, double Vo,
-                   double FI,
-                   double time_limit, double step_size, double damping_coefficent, int number_of_files)
+                      double FI,
+                      double time_limit, double step_size, double damping_coefficent, int number_of_files)
 {
 #ifdef _WIN32
     return
             _simulate_damped_os_serial(max_amplitude, length, mass, gravity, k, Ao, Vo, FI,
-                                      time_limit, step_size, damping_coefficent, number_of_files);
+                                       time_limit, step_size, damping_coefficent, number_of_files);
 #endif
     return _simulate_damped_os_parallel_mpi(max_amplitude, length, mass, gravity, k, Ao, Vo, FI,
-                                      time_limit, step_size, damping_coefficent, number_of_files);
+                                            time_limit, step_size, damping_coefficent, number_of_files);
 }
+
 int
 elastic_pendulum(double r, double length, double mass, double gravity, double k, double Ao, double Xo,
                  double Yo,
@@ -45,8 +47,8 @@ elastic_pendulum(double r, double length, double mass, double gravity, double k,
                  double time_limit, double step_size, double damping_coefficent, int number_of_files)
 {
     return
-            _simulate_elastic_pendulum(double r, double length, double mass, double gravity, double k, double Ao, double Xo,
-    double Yo,
-    double Vo,
-    double time_limit, double step_size, double damping_coefficent, int number_of_files);
+            _simulate_elastic_pendulum(r, length, mass, gravity, k, Ao, Xo,
+                                       Yo,
+                                       Vo,
+                                       time_limit, step_size, damping_coefficent, number_of_files);
 }
--- utils.c
+++ utils.c
@@ -4,17 +4,19 @@
 
 #include "utils.h"
 #include "math.h"
+
 int
-_valid_osc(double max_amplitude, double length, double mass, double gravity, double k, double time_limit,
+_valid_osc(double x, double y, double length, double mass, double gravity, double k, double time_limit,
            double step_size,
            double damping_coefficent, int number_of_files, double Fo)
 {
+    double max_length=sqrt((x*x) + (y*y));
     if(mass<0 || k<0 || time_limit<=0 || step_size<0 || length<=0 || gravity<=0 || damping_coefficent<0 ||
        number_of_files<1)
     {
         return 0;
     }
-    if(max_amplitude>length)
+    if(max_length>length)
     {
         return -1;
     }
@@ -47,20 +49,21 @@ _dy(double dy)
 }
 
 double
-_f1(double x, double y, double dx,double tx,double ty,double k,double m,double b,double r)
+_f1(double x, double y, double dx, double tx, double ty, double k, double m, double b, double r)
 {
-    double L = sqrt(pow(x - tx, 2) + pow(y - ty, 2));
-    double s = (x - tx) / L;
-    return -(double )k / m * r * s - (float)b / m * dx;
+    double L=sqrt(pow(x - tx, 2) + pow(y - ty, 2));
+    double s=(x - tx)/L;
+    return -(double) k/m*r*s - (float) b/m*dx;
 
 }
+
 double
-_f2(double x, double y, double dy,double tx,double ty,double k,double m,double b,double r,double g)
+_f2(double x, double y, double dy, double tx, double ty, double k, double m, double b, double r, double g)
 {
     {
-        double L = sqrt(pow(x - tx, 2) + pow(y - ty, 2));
-        double c = (y - ty) / L;
+        double L=sqrt(pow(x - tx, 2) + pow(y - ty, 2));
+        double c=(y - ty)/L;
 
-        return g - (float)k / m * r * c - (float)b / m * dy;
+        return g - (float) k/m*r*c - (float) b/m*dy;
     }
 }
