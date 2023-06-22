8398e11cd610f12fafa97566b6e5ccb05ac7ec79
JonathanGhaly <jghaly00@gmail.com>
2023-06-23 02:01:16 +0300

    found error in oscpara

--- examples/main.c
+++ examples/main.c
@@ -1,6 +1,9 @@
-#include "physics.h"
+#include "../include/physics.h"
 
 int main(void)
 {
     
+damped_os_serial(10.0, 14.0, 1.0, 1.8, 1.0, -1.0, 0.0,
+                      -0.1,
+                      40, 0.1, 0.1, 3);
 }
