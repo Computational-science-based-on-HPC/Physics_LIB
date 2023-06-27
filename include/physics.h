e23ca1f62631350d45d8f4d93418c5cc53cb319b
habebaRabie <habebarabie874@gmail.com>
2023-06-27 16:27:48 +0300

    remove openmp v2

--- include/physics.h
+++ include/physics.h
@@ -33,7 +33,9 @@ damped_os_parallel_execution_time_v2(double max_amplitude, double length, double
                                      double Ao,
                                      double Vo, double FI,
                                      double time_limit, double step_size, double damping_coefficent,
-                                     int number_of_files);extern double
+                                     int number_of_files);
+                                     
+extern double
 damped_os_parallel_execution_time_v3(double max_amplitude, double length, double mass, double gravity, double k,
                                      double Ao,
                                      double Vo, double FI,
