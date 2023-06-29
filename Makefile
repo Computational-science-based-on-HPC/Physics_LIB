711448994d442d4fbd2ee1c70f4456105ec7b9b1
JonathanGhaly <jghaly00@gmail.com>
2023-06-29 23:24:16 +0300

    Created DLL

---
+++ Makefile
@@ -0,0 +1,47 @@
+CC = gcc
+MCC = mpicc
+CFLAGS = -Wall -g -c -fPIC
+LDFLAGS = -lm
+OMPFLAG = -fopenmp
+SRC_DIR = src
+OBJ_DIR = build
+INCLUDE_DIR = include
+
+OBJS = $(addprefix $(OBJ_DIR)/, physics.o oscserial.o utils.o thermoutils.o oscpara.o thermopara.o thermoserial.o)
+TARGET = libphysics.so
+
+all: $(TARGET)
+
+$(TARGET): $(OBJS)
+	$(CC) -shared -o $(TARGET)  $(OBJS) $(LDFLAGS)
+
+physics.o: $(SRC_DIR)/physics.c $(INCLUDE_DIR)/physics.h
+	$(MCC) $(CFLAGS)  $(SRC_DIR)/physics.c -o  $(OBJ_DIR)/physics.o
+
+oscserial.o:  $(SRC_DIR)/oscserial.c  $(INCLUDE_DIR)/oscserial.h
+	$(CC) $(CFLAGS)  $(SRC_DIR)/oscserial.c -o  $(OBJ_DIR)/oscserial.o
+
+utils.o:  $(SRC_DIR)/utils.c  $(INCLUDE_DIR)/utils.h
+	$(CC) $(CFLAGS)  $(SRC_DIR)/utils.c -o  $(OBJ_DIR)/utils.o
+
+thermoutils.o:  $(SRC_DIR)/thermoutils.c  $(INCLUDE_DIR)/thermoutils.h
+	$(CC) $(CFLAGS)  $(SRC_DIR)/thermoutils.c -o  $(OBJ_DIR)/thermoutils.o
+	
+oscpara.o:  $(SRC_DIR)/oscpara.c  $(INCLUDE_DIR)/oscpara.h
+	$(MCC) $(CFLAGS)  $(SRC_DIR)/oscpara.c -o  $(OBJ_DIR)/oscpara.o $(OMPFLAG)
+
+thermopara.o:  $(SRC_DIR)/thermopara.c  $(INCLUDE_DIR)/thermopara.h
+	$(MCC) $(CFLAGS)  $(SRC_DIR)/thermopara.c -o  $(OBJ_DIR)/thermopara.o $(OMPFLAG)
+
+thermoserial.o:  $(SRC_DIR)/thermoserial.c  $(INCLUDE_DIR)/thermoserial.h
+	$(CC) $(CFLAGS)  $(SRC_DIR)/thermoserial.c -o  $(OBJ_DIR)/thermoserial.o
+
+install: $(TARGET)
+	cp $(TARGET) /usr/local/lib
+	cp $(INCLUDE_DIR)/*.h /usr/local/include
+
+clean:
+	rm -f $(OBJ_DIR)/*.o $(TARGET)
+
+.PHONY: all clean install
+
--- examples/main.c
+++ examples/main.c
@@ -1,4 +1,4 @@
-#include "../include/physics.h"
+#include "physics.h"
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -7,27 +7,27 @@
 
 int main(void)
 {
-//    damped_os_parallel_execution_time_v3(10.0, 14.0, 1.0, 1.8, 1.0, -1.0, 0.0,
-//                                         -0.1,
-//                                         40, 0.01, 0.1, 3,10);
+    //    damped_os_parallel_execution_time_v3(10.0, 14.0, 1.0, 1.8, 1.0, -1.0, 0.0,
+    //                                         -0.1,
+    //                                         40, 0.01, 0.1, 3,10);
     // damped_os_serial_execution(10.0, 14.0, 1.0, 1.8, 1.0, -1.0, 0.0,
     //                            -0.1,
     //                            40000000, 0.01, 0.1, 3);
-        
-// damped_os_serial_execution(10.0, 14.0, 1.0, 1.8,  1.0,
-//                            -1.0,
-//                            0.0, -0.1,
-//                            40000000, 0.01, 0.1,
-//                           3);
+
+    // damped_os_serial_execution(10.0, 14.0, 1.0, 1.8,  1.0,
+    //                            -1.0,
+    //                            0.0, -0.1,
+    //                            40000000, 0.01, 0.1,
+    //                           3);
 
     //    heat_equation_1D_P1_MPI(0.01, 0.5, 0.05, 50);
     //    heat_equation_1D_P1_OPENMP(0.01, 0.5, 0.05, 50);
     // for(int i=1; i<= 30; i++){
-         int precision = 50;
-         heat_equation_2D_P1_MPI(0.1, 5, 0.1, 0.1, precision);
+    int precision = 50;
+    heat_equation_2D_P1_MPI(0.1, 5, 0.1, 0.1, precision);
     //     precision*=2;
     // }
-       
+
     //    heat_equation_2D_P1_OPENMP(0.1, 5, 0.1, 0.1, 50);
 
     //    heat_equation_1D_serial(0.01, 0.5, 0.05,50);
