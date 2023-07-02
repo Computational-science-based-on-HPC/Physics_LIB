d8567c38db2c63792208436983e1d4ede3c19180
phoebe-george <84939421+phoebe-george@users.noreply.github.com>
2023-07-02 20:49:56 +0300

    debug 5

--- examples/main.c
+++ examples/main.c
@@ -38,23 +38,23 @@ int main(void)
     //                           3);
 
     //    heat_equation_1D_P1_MPI(0.01, 0.5, 0.05, 50);
-        heat_equation_1D_P1_OPENMP(0.01, 0.5, 0.05, 50);
+//        heat_equation_1D_P1_OPENMP(0.01, 0.5, 0.05, 50);
     // for(int i=1; i<= 30; i++){
     //  long long precision = 50;
     //  heat_equation_2D_P1_MPI(0.1, 5, 0.1, 0.1, precision);
     //     precision*=2;
     // }
 
-        heat_equation_2D_P1_OPENMP(0.1, 5, 0.1, 0.1, 50);
+//        heat_equation_2D_P1_OPENMP(0.1, 5, 0.1, 0.1, 50);
 
         heat_equation_1D_serial(0.01, 0.5, 0.05,50);
      heat_equation_2D_serial(0.1, 5, 0.1, 0.1,50);
 
     //    heat_equation_execution_time_1D_P1_MPI(0.01, 0.5, 0.05, 50);
-        heat_equation_execution_time_1D_P1_OPENMP(0.01, 0.5, 0.05, 50);
+//        heat_equation_execution_time_1D_P1_OPENMP(0.01, 0.5, 0.05, 50);
 
 //    heat_equation_execution_time_2D_P1_MPI(0.1, 5, 0.1, 0.1, 50);
-    heat_equation_execution_time_2D_P1_OPENMP(0.1, 5, 0.1, 0.1, 50);
+//    heat_equation_execution_time_2D_P1_OPENMP(0.1, 5, 0.1, 0.1, 50);
 
         heat_equation_execution_time_1D_serial(0.01, 0.5, 0.05,50);
      heat_equation_execution_time_2D_serial(0.1, 5, 0.1, 0.1,50);
