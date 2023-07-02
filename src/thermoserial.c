0238b2d1bb12df8025014de243b090d8cb93de44
phoebe-george <84939421+phoebe-george@users.noreply.github.com>
2023-07-02 21:16:30 +0300

    edit parameter in serial 1D

--- src/thermoserial.c
+++ src/thermoserial.c
@@ -67,11 +67,11 @@ int _simulate_heat_transfer_1D_serial(double time_step, double time_limit,
     sprintf(_file_name, "simulate_heat_transfer_1D_serial_%d-%02d-%02d %02d:%02d:%02d.txt", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
     fptr = fopen(_file_name, "w");
 
-    unsigned ll numTimePoint = _cal_num_time(time_step, time_limit);
+    unsigned long long numTimePoint = _cal_num_time(time_step, time_limit);
 
     ll numSpacePoint = _cal_num_space(length, space_step);
 
-    for (unsigned ll t = 0; t < numTimePoint; t++)
+    for (unsigned long long t = 0; t < numTimePoint; t++)
     {
         for (ll x = 0; x <= numSpacePoint; x++)
         {
