8e2723c9390d13c38f86bea11c26a924c72557b5
Jonathan Ghaly <60841107+JonathanGhaly@users.noreply.github.com>
2023-07-08 03:50:28 +0200

    Fixed Heat Equation 1D

--- src/thermopara.c
+++ src/thermopara.c
@@ -44,7 +44,7 @@ long double _get_value_1D_openmp(double time_step, double space_step, ll x, unsi
 #pragma omp parallel for num_threads(THREADS) schedule(static) shared(sum, x_real, t_real, precision) private(exponential, spaceXTerm, coeff)
     for (ll k = 0; k < precision; k++)
     {
-        exponential = exp(-3 * pow(2 * (k + 1), 2) * (M_PI * M_PI * t_real) / 4);
+        exponential = exp(-3 * pow(2 * k + 1, 2) * (M_PI * M_PI * t_real) / 4);
         spaceXTerm = sin((long double)(2 * k + 1) * M_PI * x_real / 2);
         coeff = 1 / (2 * k + 1);
         sum += coeff * exponential * spaceXTerm;
